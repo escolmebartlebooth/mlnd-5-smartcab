32e033dcdb8758706a014172e6e5b66a61ac5764
escolmebartlebooth <escolme@bartlebooth.co.uk>
2016-08-01 19:34:43 +0100

    refactor: Update code to reflect coding standards
    Change all method names to lowercase_underscore format
    Change all attributes to be lowercase
    Change all class names to be CapitalisedWords

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -7,82 +7,83 @@ class QLearner(object):
     """A Q-learning object"""
     
     def __init__(self):
-        self.learningRate = 1
-        self.discountRate = 0.9
+        # initialise learning parameters
+        self.learning_rate = 1
+        self.discount_rate = 0.9
         self.epsilon = 1
-        self.qTable = {}
+        
+        # initialise q table and state, action, reward trackers
+        self.q_table = {}
         self.previous_state = ()
         self.previous_action = None
         self.previous_reward = 0
+        
+        # initialise step counter for use in decaying parameters
         self.step = 0
     
     def reset():
         pass
-        # don't reset qtable as it needs to learn...?
-        #self.previous_state = ()
-        #self.previous_action = None
-        #self.previous_reward = 0
+        # don't reset qtable as it needs to learn over all trials
+        
     
     def update(self,state,action,reward):
         # update learning rate
-        self.learningRate = 1.0 / self.step
-        # first time through
+        self.learning_rate = 1.0 / self.step
+        
         if len(self.previous_state) <> 0:
-            # if the state is not in the qtable, add it to table with zero values
-            if self.previous_state not in self.qTable:
-                self.qTable[self.previous_state] = {self.previous_action : 0}
+            # if previous state has been initialised
+            if (self.previous_state not in self.q_table):
+                # if the state is not in the qtable, add it to table with zero values
+                self.q_table[self.previous_state] = {self.previous_action : 0}
             else:
-                if self.previous_action not in self.qTable[self.previous_state]:
-                    self.qTable[self.previous_state][self.previous_action] = self.previous_reward
+                if (self.previous_action not in self.q_table[self.previous_state]):
+                    # if the state is present but the action isn't add the action
+                    self.q_table[self.previous_state][self.previous_action] = self.previous_reward
                 else:
                     # capture q(s,a) from table
-                    qsa = self.qTable[self.previous_state][self.previous_action]
-                    # if new state isn't in qtable, add it with zero values
-                    if state not in self.qTable:
+                    qsa = self.q_table[self.previous_state][self.previous_action]
+                    if (state not in self.q_table):
+                        # if new state isn't in q table, set the q(s',a') to zero
                         mqsa = 0
                     else:
+                        # otherwise get the maximal q(s',a')
                         mqsa = 0
-                        for key in self.qTable[state]:
-                            if self.qTable[state][key] > mqsa:
-                                mqsa = self.qTable[state][key]
+                        for key in self.q_table[state]:
+                            if (self.q_table[state][key] > mqsa):
+                                mqsa = self.q_table[state][key]
                     # now we have qsa and mqsa we can learn
-                    qsa = (1-self.learningRate)*qsa
-                    discountedRate = reward + (self.discountRate * mqsa)
-                    qsa = qsa + (self.learningRate * discountedRate)
-                    # update qTable
-                    self.qTable[self.previous_state][self.previous_action] = qsa
-        # can update previous state, action, reward
+                    # (1-alpha)*Q(s,a)+alpha * [reward + gamma * Qmax(s',a')]
+                    qsa = (1-self.learning_rate)*qsa
+                    discounted_rate = reward + (self.discount_rate * mqsa)
+                    qsa = qsa + (self.learning_rate * discounted_rate)
+                    # update q Table with new estimate for q(s,a)
+                    self.q_table[self.previous_state][self.previous_action] = qsa
+        # update previous state, action, reward for next pass
         self.previous_state = state
         self.previous_action = action
         self.previous_reward = reward
-                                
-                        
-               
-        # capture the maximum Q(s',a') value
-        
-        # using the 2 Q values and other params, calc new Q(s,a) using:
-        # (1-alpha)*Q(s,a)+alpha * [reward + gamma * Qmax(s',a')]
-        # update q table with that value
     
     def get_action(self,state):
-        # should select best action based on greedy epsilon etc
+        # should select best action based on greedy epsilon approach
         # if random action, choose random else choose best
+    
         # update epsilon
         self.epsilon = 1.0 / self.step
-        if random.randint(0,10) > (self.epsilon*10):
+
+        if random.randint(0,10) > (self.epsilon*1000):
             # pick best
-            if state in self.qTable:
+            if (state in self.q_table):
                 # get best action if any present
                 # do this by iterating the actions dictionary assoicated with the state key and returning max value
-                maxkey = None
-                maxvalue = 0
-                for key in self.qTable[state]:
-                    if self.qTable[state][key] > maxvalue:
-                        maxkey = key
-                        maxvalue = self.qTable[state][key]
-                return maxkey
+                max_key_value = None
+                max_qsa_value = 0
+                for key in self.q_table[state]:
+                    if self.q_table[state][key] > max_qsa_value:
+                        max_key_vaue = key
+                        max_qsa_value = self.q_table[state][key]
+                return max_key_value
             else:
-                # state not in qTable so return None
+                # state not in q_table so return None
                 return 'random'
         else:
             # pick random triggered by epsilon
@@ -98,21 +99,27 @@ class LearningAgent(Agent):
         self.color = 'red'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
         # TODO: Initialize any additional variables here
+        
         # set a run type to allow for different states
         # this should be overridden in run()
         self.run_type = 'random'
+
         # override state variable initialised to None as empty tuple
         self.state = ()
-        # create a q-learner
+
+        # create a q-learner class
         self.q_learner = QLearner()
+        
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
         # TODO: Prepare for a new trip; reset any variables here, if required
-        # reset state variable initialised to empty tuple
+        
+        # reset state variable initialised to empty tuple for each initial step of a trial
         self.state = ()
         # don't reset the qlearner as it learns over each trial
-        # self.qLearner.reset
+        
+       
         
     def update(self, t):
         # Gather inputs
@@ -121,50 +128,55 @@ class LearningAgent(Agent):
         deadline = self.env.get_deadline(self)
 
         # TODO: Update state
-        # initial view of state takes all inputs
-        # 26-07: Update to remove right as it doesn't add value
-        # 27-07: removed all cars input to reduce state space
+
+        # switch to allow for running in different states
         if self.run_type == 'random':
-            self.state = (self.next_waypoint,inputs['light'])
+            # if random run type, all actions chosen randomly so ignore state
+            self.state = None
         elif self.run_type == 'way_light_only':
+            # if way_light_only then combine traffic light and way point
             self.state = (self.next_waypoint,inputs['light'])
         else:
+            # otherwise incorporate the vehicle states at the intersection
             self.state = (self.next_waypoint,inputs['light'],inputs['oncoming'],inputs['left'],inputs['right'])
         
         # TODO: Select action according to your policy
-        # if run_type = 'random' don't use q_learner and set action to random...
+        
         action = None
         if self.run_type == 'random':
+            # if run type is random, set action to string random
             action = 'random'
         else:
+            # otherwise call the q learner to get the action and increment the step
             self.q_learner.step = self.q_learner.step + 1
             action = self.q_learner.get_action(self.state)
             
-        # update action to be a random choice
         if action == 'random':
-            # qtable returned random choice, so choose random 
+            # run type is random or qtable returned random choice, so choose random 
             action = random.choice(self.env.valid_actions)
+            
         # Execute action and get reward
         reward = self.env.act(self, action)
 
         # TODO: Learn policy based on state, action, reward
+
         # learn one step in arrears - assuming not a random run
         if self.run_type != 'random':
             self.q_learner.update(self.state,action,reward)
-        
-        print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
+            
+        print "LearningAgent.update(): way_point = {}, deadline = {}, inputs = {}, action = {}, reward = {}".format(self.next_waypoint,deadline, inputs, action, reward)  # [debug]
 
 
 def run():
     """Run the agent for a finite number of trials."""
     
     # create common place to set debug values
-    dbg_deadline = False
-    dbg_update_delay = 0.5
-    dbg_display = True
-    dbg_trials = 2
+    dbg_deadline = True
+    dbg_update_delay = 0.1
+    dbg_display = False
+    dbg_trials = 1
     # create switches to run as random, state1, state2
-    dbg_runtype = 'random'
+    dbg_runtype = 'way_light_vehicles'
 
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
@@ -181,7 +193,7 @@ def run():
     sim.run(n_trials=dbg_trials)  # run for a specified number of trials
     # NOTE: To quit midway, press Esc or close pygame window, or hit Ctrl+C on the command-line
 
-    print a.q_learner.qTable
+    print a.q_learner.q_table
 
 if __name__ == '__main__':
     run()
